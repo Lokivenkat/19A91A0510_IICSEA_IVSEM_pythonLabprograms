454e8a271ba7cf3c82df7cf91b3c1006b9795b9d
Loki venkat <69497567+Lokivenkat@users.noreply.github.com>
2021-05-16 10:37:58 +0530

    Add files via upload

---
+++ Exp 2.1 Distance.py
@@ -0,0 +1,13 @@
+x1=int(input('Enter the first coordinate of x'))
+x2=int(input('Enter the second coordinate of x'))
+y1=int(input('Enter the first coordinate of y'))
+y2=int(input('Enter the second coordinate of y'))
+Distance=((x2-x1)**2+(y2-y1)**2)**(0.5)
+print(Distance)
+
+#output
+#Enter the first coordinate of x 5
+#Enter the second coordinate of x 6
+#Enter the first coordinate of y 7
+#Enter the second coordinate of y 8
+#1.4142135623730951
