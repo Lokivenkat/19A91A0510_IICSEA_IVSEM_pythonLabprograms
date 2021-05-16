028c52623ffe4638d1f465e1d7c614ee73f59457
Loki venkat <69497567+Lokivenkat@users.noreply.github.com>
2021-05-16 10:25:33 +0530

    Add files via upload

---
+++ Exp 1.2 Indentation.py
@@ -0,0 +1,21 @@
+if 1:
+    print('true')
+     print('hi')
+else :
+    print('false')
+
+ #output: unexpected indent
+
+ #to correct this:
+
+if 1:
+    print('true')
+    print('hi')
+else :
+    print('false')
+
+ #output:
+  true
+   hi
+  
+        
