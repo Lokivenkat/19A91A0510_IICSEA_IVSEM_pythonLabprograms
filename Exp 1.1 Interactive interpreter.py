6262d1dbec31800ad11e6ad7b5169c1bae6b7b4d
Loki venkat <69497567+Lokivenkat@users.noreply.github.com>
2021-05-16 12:09:37 +0530

    Add files via upload

---
+++ Exp 1.1 Interactive interpreter.py
@@ -0,0 +1,15 @@
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+c=type(a)
+print("Type of a is",c)
+d=a+b
+print("sum is",d)
+e=a-b
+print("Difference is",e)
+
+ #output
+ #Enter a number 8
+ #Enter another number 6
+ #Type of a is <class 'int'>
+ #sum is 14
+ #Difference is 2
