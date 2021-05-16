e3c9a66e381c1ea3d7b3b8bbf15bae09164a8983
Loki venkat <69497567+Lokivenkat@users.noreply.github.com>
2021-05-16 11:25:43 +0530

    Add files via upload

---
+++ Exp 3.1 Ifelse.py
@@ -0,0 +1,13 @@
+Age=int(input('Enter the age:'))
+if Age>18 :
+    print("The citizen is eligible for vote")
+else :
+    print("The citizen is not elizible for vote")
+    
+#output1:
+    #Enter the age: 22
+    #The citizen is eligible for vote
+
+#output2:
+    #Enter the age: 16
+    #The citizen is not elizible for vote
